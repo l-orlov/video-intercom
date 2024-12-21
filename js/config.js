cfe4b24698e12b8fd12d96e9d46867c86b73d3fa
lorlov <lorlov@ozon.ru>
2024-12-21 20:52:50 -0300

    Fix domain name

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 'use strict';
 
 
-const appRoot = 'https://intercom.lorlovdev.ru/'; // for local: 'http://localhost:8888/'
+const appRoot = 'https://intercom.orlovdev.ru/'; // for local: 'http://localhost:8888/'
 const wsUrl = 'ws://localhost:8080'; // for local: 'ws://localhost:8080'
 const spinnerClass = 'fa fa-spinner faa-spin animated';
