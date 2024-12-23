7a192c0b7f048478f38f9e9c6bbdc06100d4bdf6
lorlov <lorlov@ozon.ru>
2024-12-23 16:46:50 -0300

    Fix client url

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 'use strict';
 
 
-const appRoot = 'https://intercom.orlovdev.ru/'; // for local: 'http://localhost:8888/'
-const wsUrl = 'wss://ws.orlovdev.ru/comm'; // for local: 'ws://localhost:8080'
+const appRoot = 'https://intercom.orlovdev.ru'; // for local: 'http://localhost:8888'
+const wsUrl = 'wss://ws.orlovdev.ru'; // for local: 'ws://localhost:8080'
 const spinnerClass = 'fa fa-spinner faa-spin animated';
